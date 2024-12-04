f618b8ee33a1f454267bd63dd08f3b3da6ab9eed
Niroshan100 <tharinduniroshan016@gmail.com>
2024-12-04 18:18:54 +0530

    finished the categoryRoute parth

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,6 +2,9 @@ import express from "express";
 import {
   createCategory,
   deleteCategory,
+  getCategory,
+  getCategoryByName,
+  updateCategory,
 } from "../controllers/categoryController.js";
 
 const categoryRouter = express.Router();
@@ -9,5 +12,11 @@ const categoryRouter = express.Router();
 categoryRouter.post("/", createCategory);
 
 categoryRouter.delete("/:name", deleteCategory);
+
+categoryRouter.get("/:name", getCategoryByName);
+
+categoryRouter.get("/", getCategory);
+
+categoryRouter.put("/:name", updateCategory);
 
 export default categoryRouter;
